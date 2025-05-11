015556407d8926d987cc948bb9d08939554483c2
Atsuhiro <atcheri@gmail.com>
2025-05-11 15:05:58 +0200

    feat: add metatags in head component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Roboto_Slab, Roboto_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
+import manifest from "./manifest.json";
 
 const robotoSans = Roboto_Slab({
   variable: "--font-roboto-sans",
@@ -23,8 +25,27 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { name: title, description } = manifest;
   return (
     <html lang="en">
+      <Head>
+        <title>{title}</title>
+        <meta property="og:title" content="A-T-C-H" key="title" />
+        <meta name="description" content={description} />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        <link rel="mask-icon" href="/itsutsu-inazuma.svg" color="#362F78"></link>
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content="/qui-suis-je-hero.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content="/qui-suis-je-hero.png" />
+        {/* <meta name="twitter:site" content= />
+        <meta name="twitter:creator" content= /> */}
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body className={`${robotoSans.variable} ${robotoMono.variable} antialiased`}>{children}</body>
     </html>
   );
