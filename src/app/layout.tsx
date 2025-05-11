b55f8072952cbaaf77e319c0a70aeadce2cde1c8
Atsuhiro <atcheri@gmail.com>
2025-05-11 15:18:33 +0200

    fix: put the metadata inside of the metadata object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Roboto_Slab, Roboto_Mono } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
-import manifest from "./manifest.json";
 
 const robotoSans = Roboto_Slab({
   variable: "--font-roboto-sans",
@@ -14,10 +12,32 @@ const robotoMono = Roboto_Mono({
   subsets: ["latin"],
 });
 
+const commonTitle = "Qui-suis je? Atch";
+const commonDescription = "Non, ce n'est pas un site de rencontre.";
+const commonImage = "/qui-suis-je-hero.png";
+
 export const metadata: Metadata = {
-  title: "Qui-suis je? Atch",
-  description: "Non, ce n'est pas un site de rencontre.",
+  title: commonTitle,
+  description: commonDescription,
   manifest: "/manifest.json",
+  themeColor: "#362F78",
+  icons: {
+    icon: [
+      { url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  openGraph: {
+    title: commonTitle,
+    description: commonDescription,
+    images: [commonImage],
+  },
+  twitter: {
+    title: commonTitle,
+    description: commonDescription,
+    images: [commonImage],
+  },
 };
 
 export default function RootLayout({
@@ -25,27 +45,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { name: title, description } = manifest;
   return (
     <html lang="en">
-      <Head>
-        <title>{title}</title>
-        <meta property="og:title" content="A-T-C-H" key="title" />
-        <meta name="description" content={description} />
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
-        <link rel="mask-icon" href="/itsutsu-inazuma.svg" color="#362F78"></link>
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content="/qui-suis-je-hero.png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content="/qui-suis-je-hero.png" />
-        {/* <meta name="twitter:site" content= />
-        <meta name="twitter:creator" content= /> */}
-        <link rel="manifest" href="/manifest.json" />
-      </Head>
       <body className={`${robotoSans.variable} ${robotoMono.variable} antialiased`}>{children}</body>
     </html>
   );
